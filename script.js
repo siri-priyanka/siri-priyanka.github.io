3d4780ff39bf27032d867eaef37a21e28d696356
siri-priyanka <siripriyankatalla612@gmail.com>
2025-09-01 11:48:25 +0530

    Adding both css and js files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio Loaded ✅");
+
+  // Example: Welcome alert (optional)
+  alert("Welcome to Siri Priyanka's Portfolio 🚀");
+});
